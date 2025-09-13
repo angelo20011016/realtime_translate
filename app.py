6633cad5216247b67859e59acdc91d169ee1a705
angelo20011016 <angelocandu1@gmail.com>
2025-09-13 11:11:53 +0800

    動態調整render/local啟動的auth url

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def login_page():
 
 @app.route('/login')
 def login():
-    redirect_uri = 'https://happywecan.com/authorize'
+    redirect_uri = os.getenv('REDIRECT_URI', 'http://localhost:5000/authorize')
     return google.authorize_redirect(redirect_uri)
 
 @app.route('/authorize')
